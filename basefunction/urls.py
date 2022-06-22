9dfbe7c32734902a246e21c625fed0e0804e8192
takaki.ishikawa <rittukk1@gmail.com>
2022-06-22 23:18:45 +0900

    [修正]

--- basefunction/urls.py
+++ basefunction/urls.py
@@ -9,8 +9,7 @@ urlpatterns=[
     path("top/",views.topView.as_view(),name="top"),
     path("guide/",views.guideView.as_view(),name="guide"),
     path("users/",views.usersView.as_view(),name="users"),
-    path("update/",views.GanspaUpdateView.as_view(),name="update"),
     path("register/",views.registerView.as_view(),name="register"),
-    path("delete/<int:pk>",views.GanspaDeleteView.as_view(), name="delete"),
-    #    path("sample/",views.LoginView.as_view(),name="sample"),
+    path("delete/<int:pk>",views.GanDeleteView.as_view(), name="delete"),
+    path("update/<str:pk>",views.GanUpdateView.as_view(),name="update"),
 ]
